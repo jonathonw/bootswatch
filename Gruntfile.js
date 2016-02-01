295f69542ae585e573ba47d93febade06b137814
Jonathon Williams <gandalf72487@yahoo.com>
2016-01-31 22:20:11 -0600

    Add a lightly modified flatly theme

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function (grunt) {
             '*/\n',
     swatch: {
       amelia:{}, cerulean:{}, cosmo:{}, cyborg:{}, darkly:{},
-      flatly:{}, journal:{}, lumen:{}, paper:{}, readable:{},
+      flatly:{}, journal:{}, lumen:{}, paper:{}, readable:{}, sanctuary:{},
       sandstone:{}, simplex:{}, slate:{}, spacelab:{}, superhero:{},
       united:{}, yeti:{}, custom:{}
     },
